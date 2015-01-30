e9e6288f505da739f3dbbe18f5fa563a97afae25
Vikas Pardeshi <embededftl@gmail.com>
2015-01-30 10:41:18 +0530

    yfhjf

---
+++ src/userclasses/StateMachine.java
@@ -0,0 +1,22 @@
+/**
+ * Your application code goes here
+ */
+
+package userclasses;
+
+import generated.StateMachineBase;
+import com.codename1.ui.*; 
+import com.codename1.ui.events.*;
+import com.codename1.ui.util.Resources;
+
+
+public class StateMachine extends StateMachineBase {
+    public StateMachine(String resFile) {
+        super(resFile);
+     
+    }
+    /*add comment
+  */
+	protected void initVars(Resources res){
+	}
+}
